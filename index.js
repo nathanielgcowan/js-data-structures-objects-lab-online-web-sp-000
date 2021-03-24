3c74c16a94bbc7586a47bdd31e6380a533ec4624
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-03-24 20:49:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign({},driver, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, potato) {
+    driver[key] = potato;
+
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+    const newObjy = Object.assign({}, driver);
+    delete newObjy[key];
+    return newObjy;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
